16308583d6238c79925409b4fb951efb274b9b3b
Lucas Marandat <lucas.mrdt@gmail.com>
2019-06-20 01:28:11 +0200

    fix prepare_env.sh

--- client/scripts/prepare_env.sh
+++ client/scripts/prepare_env.sh
@@ -2,15 +2,16 @@
 
 IFS=
 ENV_FILE=".env"
-IP_ENV_FORMAT="IP=%s\n"
+ENV_KEY="REACT_APP_IP"
+IP_ENV_FORMAT="$ENV_KEY=%s\n"
 PRIVATE_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
 NEW_ENV_VARIABLE=$(printf $IP_ENV_FORMAT $PRIVATE_IP)
 
 if [ ! -f $ENV_FILE ]; then
   echo $NEW_ENV_VARIABLE > $ENV_FILE
-elif [ $(grep -Eo 'IP=' $ENV_FILE) ]; then
-  NEW_ENV_FILE=$(sed "s/IP=.*/$NEW_ENV_VARIABLE/" $ENV_FILE)
+elif [ $(grep -Eo "$ENV_KEY=" $ENV_FILE) ]; then
+  NEW_ENV_FILE=$(sed "s/$ENV_KEY=.*/$NEW_ENV_VARIABLE/" $ENV_FILE)
   echo $NEW_ENV_FILE > $ENV_FILE
 else
   echo $NEW_ENV_VARIABLE >> $ENV_FILE
