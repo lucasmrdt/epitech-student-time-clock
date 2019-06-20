ff97f57d0475378f24059ceb0869d467c94c933e
Lucas Marandat <lucas.mrdt@gmail.com>
2019-06-20 13:37:46 +0200

    fix

--- client/src/constants/network.js
+++ client/src/constants/network.js
@@ -5,7 +5,7 @@ export const GET_PICTURE_URL_FROM_LOGIN = (login: string) => (
 );
 
 export const SERVER_PORT = 1234;
-export const TOGGLE_BREAK_URL = `http://${process.env.REACT_APP_IP}:${SERVER_PORT}/break/toggle`;
-export const REMOVE_BREAK_URL = `http://${process.env.REACT_APP_IP}:${SERVER_PORT}/break/remove`;
-export const COMMENT_URL = `http://${process.env.REACT_APP_IP}:${SERVER_PORT}/comment`;
-export const LOGINS_URL = `http://${process.env.REACT_APP_IP}:${SERVER_PORT}/logins`
+export const TOGGLE_BREAK_URL = `http://localhost:${SERVER_PORT}/break/toggle`;
+export const REMOVE_BREAK_URL = `http://localhost:${SERVER_PORT}/break/remove`;
+export const COMMENT_URL = `http://localhost:${SERVER_PORT}/comment`;
+export const LOGINS_URL = `http://localhost:${SERVER_PORT}/logins`
